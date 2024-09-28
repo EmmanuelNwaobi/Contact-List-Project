e449e27da5ca0e17d7ed2af7eb5ffc9f17faf0b5
EmmanuelNwaobi <emmanueltommy51@gmail.com>
2024-09-28 23:07:06 +0100

    recent 0

---
+++ index.js
@@ -0,0 +1,138 @@
+let form = document.getElementById("form")
+let nameGroup = document.getElementById("name")
+let addressGroup = document.getElementById("address")
+let numberGroup = document.getElementById("number")
+let availableContactContainer = document.getElementById("available-contacts")
+let mainCardContainer = document.getElementById("card-container")
+let actionMessage = document.getElementById("action__message")
+
+
+let contactArray = []
+
+// Collect user's input data 
+form.addEventListener("submit", collectData)
+function collectData(event){
+    event.preventDefault()
+
+    let nameInput = nameGroup.value
+    let addressInput = addressGroup.value
+    let numberInput = numberGroup.value
+
+    if(nameInput.length === 0 || addressInput.length === 0 || numberInput.length === 0){
+        alert("All field's must be completed")
+        return
+    }else {
+        const userInput = {
+            nameOfContact : nameInput,
+            addressOfContact : addressInput,
+            numberOfContact : numberInput
+        }
+        contactArray.push(userInput)
+        actionMessage.style.display = "block"
+    }
+    localStorage.setItem("contactInfo", JSON.stringify(contactArray))
+    form.reset()
+    fetchUserData()
+}
+
+// Fetch user data from Local Storage 
+function fetchUserData(){
+    if(localStorage.getItem("contactInfo")){
+        contactArray = JSON.parse(localStorage.getItem("contactInfo"))
+    }
+    printContactInfo()
+}
+fetchUserData()
+
+// Print contact Information to the UI
+function printContactInfo(){
+    if(contactArray.length === 0){
+        availableContactContainer.style.display = "none"
+    }else{
+        availableContactContainer.style.display = "flex"
+    }
+
+    mainCardContainer.innerHTML = ""
+    contactArray.forEach((item, index)=> {
+        let contactName = item.nameOfContact
+        let contactAddress = item.addressOfContact
+        let contactNumber = item.numberOfContact
+
+        let cardDiv = document.createElement("div") // main card 
+        cardDiv.classList.add("card")
+
+        let contactIdDiv = document.createElement("div")
+        contactIdDiv.classList.add("contact-id")
+
+        let headingText = document.createElement("h4")
+        headingText.textContent = "Contact ID:"
+
+        let pTag = document.createElement("p")
+        pTag.textContent = `${index}`
+
+        contactIdDiv.append(headingText, pTag)
+
+        let contactIdDivName = document.createElement("div")
+        contactIdDivName.classList.add("contact-id")
+
+        let headingTextName = document.createElement("h4")
+        headingTextName.textContent = "Name:"
+
+        let pTagName = document.createElement("p")
+        pTagName.textContent = contactName 
+
+        contactIdDivName.append(headingTextName, pTagName)
+
+        let contactIdDivAddress = document.createElement("div")
+        contactIdDivAddress.classList.add("contact-id")
+
+        let headingTextAddress = document.createElement("h4")
+        headingTextAddress.textContent = "Address:"
+
+        let pTagNameAddress = document.createElement("p")
+        pTagNameAddress.textContent = `${contactAddress}` 
+
+        contactIdDivAddress.append(headingTextAddress, pTagNameAddress)
+
+        let contactIdDivNumber = document.createElement("div")
+        contactIdDivNumber.classList.add("contact-id")
+
+        let headingTextNumber = document.createElement("h4")
+        headingTextNumber.textContent = "Contact Number:"
+
+        let pTagNameNumber = document.createElement("p")
+        pTagNameNumber.textContent = contactNumber 
+
+        contactIdDivNumber.append(headingTextNumber, pTagNameNumber)
+
+        let actionBtnDiv = document.createElement("div") //main button container
+        actionBtnDiv.classList.add("action-btn")
+
+        let deleteButtonDiv = document.createElement("div")
+        deleteButtonDiv.classList.add("action-btn__delete")
+
+        let deleteIcon = document.createElement("i")
+        deleteIcon.classList.add("fa-solid", "fa-trash")
+
+        let deleteText = document.createElement("p")
+        deleteText.textContent = "Delete"
+
+        deleteButtonDiv.append(deleteIcon, deleteText)
+
+        let editButtonDiv = document.createElement("div")
+        editButtonDiv.classList.add("action-btn__edit")
+
+        let editIcon = document.createElement("i")
+        editIcon.classList.add("fa-solid", "fa-pen-to-square")
+
+        let editText = document.createElement("p")
+        editText.textContent = "Edit"
+
+        editButtonDiv.append(editIcon, editText)
+        actionBtnDiv.append(deleteButtonDiv, editButtonDiv)
+
+        cardDiv.append(contactIdDiv, contactIdDivName, contactIdDivAddress, contactIdDivNumber, actionBtnDiv)
+        mainCardContainer.append(cardDiv)
+
+    })
+}
